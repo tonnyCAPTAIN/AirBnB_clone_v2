02b3d6759d617a7225131bec1428560ee1eacd61
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2023-01-15 05:05:05 -0800

    task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,16 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
-            return self.__objects
-        cls_name = cls.__name__
-        dct = {}
-        for key in self.__objects.keys():
-            if key.split('.')[0] == cls_name:
-                dct[key] = self.__objects[key]
-        return dct
+        if (cls is None):
+            return FileStorage.__objects
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            if (value.__class__ == cls):
+                new_dict[key] = value
+        return (new_dict)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -43,25 +42,27 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        ''' deletes the object obj from the attribute
-            __objects if it's inside it
-        '''
-        if obj is None:
-            return
-        obj_key = obj.to_dict()['__class__'] + '.' + obj.id
-        if obj_key in self.__objects.keys():
-            del self.__objects[obj_key]
+        """Method to delete class"""
+        if (obj is None):
+            pass
+
+        if (obj in FileStorage.__objects.values()):
+            key = obj.__class__.__name__ + "." + obj.id
+            del (FileStorage.__objects[key])
+    def close(self):
+        """calls the reload method"""
+        self.reload()
--- models/state.py
+++ models/state.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models import storage_type
+from models.base_model import BaseModel
+from models.base_model import Base
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+#from models import storage
+from os import getenv
 
 class State(BaseModel, Base):
-    """ State class / table model"""
-    __tablename__ = 'states'
-    if storage_type == 'db':
+    """ State class """
+    __tablename__ = "states"
+    if (getenv("HBNB_TYPE_STORAGE") == "db"):
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
+        cities = relationship('City', backref='state')
     else:
-        name = ''
-
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
+            """returns the list of city instance with sate_id
+            equals the current state.id
+            fileStorage relationship between state and city
+            """
             related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+            objects = storage.all(City)
+            for key, value in objects.items():
+                if value.state_id == self.id:
+                    related_cities.append(value)
+                return related_cities
